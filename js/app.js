ea1639cbe0c3bb107a4ebfd484e4475c7d4292a9
Jakub Kowalczyk <jakub.kowalczyk@urtica.pl>
2018-12-16 11:44:24 +0100

    layout done TODO: logo

--- js/app.js
+++ js/app.js
@@ -14,6 +14,13 @@ const offerTools = document.getElementById("offer-tools");
 const offerUpholstering = document.getElementById("offer-upholstering");
 const offerLeathers = document.getElementById("offer-leathers");
 const offerOther = document.getElementById("offer-other");
+const contactHeader = document.getElementById("contact-header");
+const businessCardContainer = document.getElementById("business-card-container");
+const businessCard = document.getElementById("business-card");
+const map = document.getElementById("map");
+const copyright = document.getElementById("copyright");
+const footer = document.getElementById("footer");
+
 
 function animate() {
     aboutHeader1.classList.add("show-element");
@@ -30,52 +37,68 @@ function animate() {
         aboutHeaderImage.classList.add("move-element-left")
     }, 200);
 
-
     aboutDescriptionImage.classList.add("show-element");
     aboutDescription1.classList.add("move-element-up");
     setTimeout(function () {
         aboutDescription2.classList.add("move-element-up")
     }, 200);
 
-    setTimeout(function () {
-        offerPursemaking.classList.add("show-element")
-    }, 200);
-    setTimeout(function () {
-        offerShoemaking.classList.add("show-element")
-    }, 400);
-    setTimeout(function () {
-        offerTools.classList.add("show-element")
-    }, 600);
-    setTimeout(function () {
-        offerUpholstering.classList.add("show-element")
-    }, 800);
-    setTimeout(function () {
-        offerLeathers.classList.add("show-element")
-    }, 1000);
-    setTimeout(function () {
-        offerOther.classList.add("show-element")
-    }, 1200);
 
 }
 
 function onScroll() {
+    const siteHeight = document.body.scrollHeight;
     const clientHeight = document.documentElement.clientHeight;
     const scroll = window.pageYOffset || document.documentElement.scrollTop;
     const windowPosition = clientHeight + scroll;
+    const bodyRectTop = document.body.getBoundingClientRect().top;
 
-
-    let bodyRectTop = document.body.getBoundingClientRect().top;
-    let offerHeaderTop = offerHeader.getBoundingClientRect().top - bodyRectTop;
-    console.log(windowPosition);
-    console.log(offerHeaderTop);
     if (scroll > 10) {
         nav.classList.add("nav-shadow");
     } else {
-        nav.classList.remove("nav-shadow")
+        nav.classList.remove("nav-shadow");
     }
 
+    const offerHeaderTop = offerHeader.getBoundingClientRect().top - bodyRectTop;
     if (windowPosition > offerHeaderTop) {
         offerHeader.classList.add("move-element-up");
+    }
+
+    const offerPursemakingTop = offerPursemaking.getBoundingClientRect().top - bodyRectTop;
+    const offerToolsTop = offerTools.getBoundingClientRect().top - bodyRectTop;
+    const offerLeathersTop = offerLeathers.getBoundingClientRect().top - bodyRectTop;
+    if (windowPosition > offerPursemakingTop) {
+        offerPursemaking.classList.add("show-element");
+        setTimeout(function () {
+            offerShoemaking.classList.add("show-element")
+        }, 200);
+    }
+    if (windowPosition > offerToolsTop) {
+        offerTools.classList.add("show-element");
+        setTimeout(function () {
+            offerUpholstering.classList.add("show-element")
+        }, 200);
+    }
+    if (windowPosition > offerLeathersTop) {
+        offerLeathers.classList.add("show-element");
+        setTimeout(function () {
+            offerOther.classList.add("show-element")
+        }, 200);
+    }
+
+    const contactHeaderTop = contactHeader.getBoundingClientRect().top - bodyRectTop;
+    if (windowPosition > contactHeaderTop) {
+        contactHeader.classList.add("move-element-up");
+        setTimeout(function () {
+            businessCard.classList.add("show-element")
+        }, 200);
+        setTimeout(function () {
+            map.classList.add("show-element")
+        }, 400);
+    }
+
+    const businessCardContainerTop = businessCardContainer.getBoundingClientRect().top - bodyRectTop;
+    if (windowPosition > businessCardContainerTop) {
     }
 }
 
