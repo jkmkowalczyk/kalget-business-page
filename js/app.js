8c42c5bffc49a156219e0009ace5ee4aae0171d2
Jakub Kowalczyk <jakub.kowalczyk@urtica.pl>
2018-12-22 13:09:47 +0100

    menu + slider

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
+// elements
 const nav = document.getElementsByTagName("nav")[0];
+const homeLink = document.getElementById("home-link");
+const aboutLink = document.getElementById("about-link");
+const offerLink = document.getElementById("offer-link");
+const contactLink = document.getElementById("contact-link");
+const localisationLink = document.getElementById("localisation-link");
 const aboutHeader = document.getElementById("about-header");
 const aboutHeader1 = document.getElementById("about-header1");
 const aboutHeader2 = document.getElementById("about-header2");
 const aboutHeader3 = document.getElementById("about-header3");
 const aboutHeader4 = document.getElementById("about-header4");
 const aboutHeaderImage = document.getElementById("about-header-image");
+const aboutSection = document.getElementById("about-description-container");
 const aboutDescriptionImage = document.getElementById("about-description-image");
 const aboutDescription1 = document.getElementById("about-description1");
 const aboutDescription2 = document.getElementById("about-description2");
+const offerSection = document.getElementById("offer");
 const offerHeader = document.getElementById("offer-header");
 const offerPursemaking = document.getElementById("offer-pursemaking");
 const offerShoemaking = document.getElementById("offer-shoemaking");
@@ -18,14 +26,61 @@ const offerOther = document.getElementById("offer-other");
 const contactHeader = document.getElementById("contact-header");
 const businessCardContainer = document.getElementById("business-card-container");
 const businessCard = document.getElementById("business-card");
+const contactSection = document.getElementById("contact");
 const map = document.getElementById("map");
 const copyright = document.getElementById("copyright");
 const footer = document.getElementById("footer");
 const menu = document.getElementById("menu");
 const menuList = document.getElementById("menu-list");
 
+// positions
+const siteHeight = document.body.scrollHeight;
+const clientHeight = document.documentElement.clientHeight;
 
-function animate() {
+const bodyRectTop = document.body.getBoundingClientRect().top;
+const aboutSectionTop = aboutSection.getBoundingClientRect().top - bodyRectTop;
+const offerSectionTop = offerSection.getBoundingClientRect().top - bodyRectTop;
+const contactSectionTop = contactSection.getBoundingClientRect().top - bodyRectTop;
+
+
+function setActiveLink() {
+    const scroll = window.pageYOffset + 1 || document.documentElement.scrollTop + 1;
+    if (scroll < aboutSectionTop) {
+        homeLink.classList.add("active-link");
+    } else {
+        homeLink.classList.remove("active-link");
+    }
+
+    if (scroll >= aboutSectionTop && scroll < offerSectionTop) {
+        aboutLink.classList.add("active-link");
+    } else {
+        aboutLink.classList.remove("active-link");
+    }
+    if (scroll >= offerSectionTop && scroll < contactSectionTop) {
+        offerLink.classList.add("active-link");
+    } else {
+        offerLink.classList.remove("active-link");
+    }
+
+    if (scroll >= contactSectionTop) {
+        contactLink.classList.add("active-link");
+    } else {
+        contactLink.classList.remove("active-link");
+    }
+}
+
+
+function onScroll() {
+    const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    const windowPosition = clientHeight + scroll;
+    setActiveLink();
+
+    if (scroll > 10) {
+        nav.classList.add("nav-shadow");
+    } else {
+        nav.classList.remove("nav-shadow");
+    }
+
     aboutHeader.classList.add("show-element");
     aboutHeader1.classList.add("show-element");
     setTimeout(function () {
@@ -46,22 +101,6 @@ function animate() {
     setTimeout(function () {
         aboutDescription2.classList.add("move-element-up")
     }, 200);
-
-
-}
-
-function onScroll() {
-    const siteHeight = document.body.scrollHeight;
-    const clientHeight = document.documentElement.clientHeight;
-    const scroll = window.pageYOffset || document.documentElement.scrollTop;
-    const windowPosition = clientHeight + scroll;
-    const bodyRectTop = document.body.getBoundingClientRect().top;
-
-    if (scroll > 10) {
-        nav.classList.add("nav-shadow");
-    } else {
-        nav.classList.remove("nav-shadow");
-    }
 
     const offerHeaderTop = offerHeader.getBoundingClientRect().top - bodyRectTop;
     if (windowPosition > offerHeaderTop) {
@@ -106,7 +145,6 @@ function onScroll() {
     }
 }
 
-
 function toggleMenu() {
     if (menu.classList.contains("open")) {
         menu.classList.remove("open");
@@ -114,10 +152,32 @@ function toggleMenu() {
     } else {
         menu.classList.add("open");
         menuList.classList.add("show-menu");
-        console.log("add show-menu");
     }
 }
 
-window.onload = animate;
+window.addEventListener("load", setActiveLink);
+window.addEventListener("load", onScroll);
 window.addEventListener("scroll", onScroll);
 menu.addEventListener("click", toggleMenu);
+
+
+function scrollHome() {
+    window.scrollTo(0, 0);
+}
+
+function scrollAbout() {
+    window.scrollTo(0, aboutSectionTop);
+}
+
+function scrollOffer() {
+    window.scrollTo(0, offerSectionTop);
+}
+
+function scrollContact() {
+    window.scrollTo(0, contactSectionTop);
+}
+
+homeLink.addEventListener("click", scrollHome);
+aboutLink.addEventListener("click", scrollAbout);
+offerLink.addEventListener("click", scrollOffer);
+contactLink.addEventListener("click", scrollContact);
